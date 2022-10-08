0fa77bda0afff070bd12a214c16413b7c69b6913
sachinPunia <64972820+sachinPunia@users.noreply.github.com>
2022-10-09 00:38:59 +0530

    Update songs-list.js

--- js/audio-lists/songs-list.js
+++ js/audio-lists/songs-list.js
@@ -3,63 +3,46 @@ let BASE_URL_IMAGES = './resources/audio/music/images';
 
 export const musicLibrary = [
     {
-        name: "The Lounge",
-        path: `${BASE_URL_SONGS}/thelounge.mp3`,
-        img: `${BASE_URL_IMAGES}/thelounge.jpg`,
-        singer: "Loungeman Greyham"
-    },
-    {
-        name: "Creepy",
-        path: `${BASE_URL_SONGS}/creepy.mp3`,
-        img: `${BASE_URL_IMAGES}/creepy.jpg`,
-        singer: "Dr. Creep X"
-    },
-    {
-        name: "Energy",
-        path: `${BASE_URL_SONGS}/energy.mp3`,
-        img: `${BASE_URL_IMAGES}/energy.jpg`,
-        singer: "Popeye Sailorman"
-    },
-    {
-        name: "Epic",
-        path: `${BASE_URL_SONGS}/epic.mp3`,
-        img: `${BASE_URL_IMAGES}/epic.jpg`,
-        singer: "Thorin Oakenshield"
-    },
-    {
-        name: "Erf",
-        path: `${BASE_URL_SONGS}/erf.mp3`,
+        name: "Krishna Flute",
+        path: `${BASE_URL_SONGS}/Krishna.mp3`,
         img: `${BASE_URL_IMAGES}/erf.jpg`,
-        singer: "Dobby the Elf"
+        singer: "Rakesh Chaurasia"
     },
     {
-        name: "Groovy HipHop",
-        path: `${BASE_URL_SONGS}/groovyhiphop.mp3`,
+        name: "Ekadantaya",
+        path: `${BASE_URL_SONGS}/Ekadantaya.mp3`,
         img: `${BASE_URL_IMAGES}/groovyhiphop.jpg`,
-        singer: "Kanye East"
+        singer: "Shankar Mahadevan"
     },
     {
-        name: "Moose",
-        path: `${BASE_URL_SONGS}/moose.mp3`,
+        name: "Classical Song",
+        path: `${BASE_URL_SONGS}/Classical.mp3`,
+        img: `${BASE_URL_IMAGES}/energy.jpg`,
+        singer: "Udit Narayan"
+    },
+    {
+        name: "Mere Dholna",
+        path: `${BASE_URL_SONGS}/Meredholna.mp3`,
+        img: `${BASE_URL_IMAGES}/epic.jpg`,
+        singer: "Pritam Chakraborty"
+    },
+    {
+        name: "Relax Music",
+        path: `${BASE_URL_SONGS}/relax.mp3`,
+        img: `${BASE_URL_IMAGES}/erf.jpg`,
+        singer: "Udit Narayan"
+    },
+    {
+        name: "Shape of you",
+        path: `${BASE_URL_SONGS}/soy.mp3`,
+        img: `${BASE_URL_IMAGES}/groovyhiphop.jpg`,
+        singer: "Indian Style"
+    },
+    {
+        name: "Mohe Rang",
+        path: `${BASE_URL_SONGS}/moherang.mp3`,
         img: `${BASE_URL_IMAGES}/moose.jpg`,
-        singer: "Gellert Grindelwald"
+        singer: "Birju Maharaj"
     },
-    {
-        name: "Once again",
-        path: `${BASE_URL_SONGS}/onceagain.mp3`,
-        img: `${BASE_URL_IMAGES}/onceagain.jpg`,
-        singer: "Samwell Tarly"
-    },
-    {
-        name: "Psychedelic",
-        path: `${BASE_URL_SONGS}/psychedelic.mp3`,
-        img: `${BASE_URL_IMAGES}/psychedelic.jpg`,
-        singer: "Trippy Jay"
-    },
-    {
-        name: "Sunny",
-        path: `${BASE_URL_SONGS}/sunny.mp3`,
-        img: `${BASE_URL_IMAGES}/sunny.jpg`,
-        singer: "Bugs Bunny"
-    },
+
 ];
